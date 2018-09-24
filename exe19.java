045ac7209ff90e9b1fb24a880985d020f910407b
SathishRamadoss <40665079+SathishRamadoss@users.noreply.github.com>
2018-09-24 06:46:44 +0530

    Add files via upload

---
+++ exe19.java
@@ -0,0 +1,17 @@
+//example of this keyword  
+		class Exp19{  
+		    int id;  
+		    String name;  
+		
+		    Exp19(int id,String name){  
+		    this.id = id;  
+		    this.name = name;  
+		    }  
+		    void display(){System.out.println(id+""+name);}  
+		    public static void main(String args[]){  
+		    Exp19 s1 = new Exp19(111,"Deva");  
+	            Exp19 s2 = new Exp19(222,"Alai");  
+		    s1.display();  
+		    s2.display();  
+		}  
+		}  
---
+++ exe2.java
@@ -0,0 +1,13 @@
+package lab_programs;
+         
+public class ex02 {
+	 
+	int id;//data member (also instance variable)  
+	 String name;//data member(also instance variable) 
+	 public static void main(String args[]){  
+	  ex02 s1=new ex02();//creating an object of Student  
+	  System.out.println(s1.id);  
+	  System.out.println(s1.name);  
+
+}
+}
